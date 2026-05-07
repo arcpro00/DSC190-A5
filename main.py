4882474fb6a1435c7c3c6936ea38f330f174fc31
arcpro00 <aedantpt@gmail.com>
2026-05-06 18:11:09 -0700

    Test pre-commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def main():
-    '''Says hello to the project'''
-    print('Hello from dsc190-a5!')
+    """Says hello to the project"""
+    print("Hello from dsc190-a5!")
+
+
 if __name__ == "__main__":
     main()
+x = 1 + 2
