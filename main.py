863ab439483759fc2de2ce4f22910cead11fcf9d
arcpro00 <aedantpt@gmail.com>
2026-05-06 18:04:46 -0700

    fixed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    f'Says hello to the project'
-    	print('Hello from dsc190-a5!')
+    '''Says hello to the project'''
+    print('Hello from dsc190-a5!')
 if __name__ == "__main__":
-main()
+    main()
